2a7efdd4ca3447262d2b285c238e6bdad611068f
astro <aj@koop.ws>
2019-01-02 10:50:10 -0500

    attempt add search func fix

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -63,9 +63,9 @@ export default {
     },
     searchedBooks() {
       const searchFilter = book => {
-        return book.title.toLowerCase().match(this.searchInput.toLowerCase);
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
       };
-      return _.filter(this.books, searchFilter);
+        return _.filter(this.books, searchFilter);
     }
   },
   methods: {
